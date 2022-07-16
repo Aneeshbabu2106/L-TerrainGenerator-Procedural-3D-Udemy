4eb02e29b3c53f38ecfbc35c5bf618c18f8ce53c
Aneesh Babu <73874288+Aneeshbabu2106@users.noreply.github.com>
2022-07-16 20:17:47 +0530

    adding CustomTerrainEditor scipt

---
+++ Assets/Editor/CustomTerrainEditor.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEditor;
+using EditorGUITable;
+
+[CustomEditor(typeof(CustomTerrain))]
+[CanEditMultipleObjects]
+public class CustomTerrainEditor : Editor
+{
+
+    private void OnEnable()
+    {
+        
+    }
+    public override void OnInspectorGUI()
+    {
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
